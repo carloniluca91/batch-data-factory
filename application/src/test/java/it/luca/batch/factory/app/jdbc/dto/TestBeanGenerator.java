d4917999faceeb5afce14281f73c0a831f4ef9ad
carloniluca91 <carloni.luca91@gmail.com>
2021-09-17 21:05:03 +0200

    generated around 100000 records of OcsRecord ... result not bad ;)

--- application/src/test/java/it/luca/batch/factory/app/jdbc/dto/TestBeanGenerator.java
+++ application/src/test/java/it/luca/batch/factory/app/jdbc/dto/TestBeanGenerator.java
@@ -3,4 +3,9 @@ package it.luca.batch.factory.app.jdbc.dto;
 import it.luca.data.factory.generator.bean.CustomGenerator;
 
 public class TestBeanGenerator implements CustomGenerator<TestBean> {
+
+    @Override
+    public TestBean setNonRandomAttributes(TestBean testBean) {
+        return null;
+    }
 }
--- data-model/src/test/java/it/luca/batch/factory/model/generation/TestBeanGenerator.java
+++ data-model/src/test/java/it/luca/batch/factory/model/generation/TestBeanGenerator.java
@@ -4,4 +4,8 @@ import it.luca.data.factory.generator.bean.CustomGenerator;
 
 public class TestBeanGenerator implements CustomGenerator<TestBean> {
 
+    @Override
+    public TestBean setNonRandomAttributes(TestBean testBean) {
+        return null;
+    }
 }
